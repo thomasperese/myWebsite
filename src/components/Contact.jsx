f6d0becc5ed963406791f8a0409b2ff5f470ee2d
ThomasPerese <thomasperese@hotmail.com>
2025-09-15 06:26:06 +1200

    Save changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,58 +1,26 @@
 import React from "react";
-import { Box, Typography, Stack, Button } from "@mui/material";
+import { Stack, Button } from "@mui/material";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import PhoneIcon from "@mui/icons-material/Phone";
 
+
 export default function Contact() {
-  return (
-    <Box>
-      <Typography variant="h6" fontWeight={600} gutterBottom>
-        Contact
-      </Typography>
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        spacing={2}
-        alignItems={{ xs: "stretch", sm: "center" }}
-      >
-        <Button
-          variant="outlined"
-          startIcon={<EmailIcon />}
-          href="mailto:thomasperese@hotmail.com"
-          fullWidth
-        >
-          thomasperese@hotmail.com
-        </Button>
-        <Button
-          variant="outlined"
-          startIcon={<LinkedInIcon />}
-          href="https://linkedin.com/in/thomas-perese-140441190"
-          target="_blank"
-          rel="noreferrer"
-          fullWidth
-        >
-          LinkedIn
-        </Button>
-        <Button
-          variant="outlined"
-          startIcon={<GitHubIcon />}
-          href="#"
-          target="_blank"
-          rel="noreferrer"
-          fullWidth
-        >
-          GitHub
-        </Button>
-        <Button
-          variant="outlined"
-          startIcon={<PhoneIcon />}
-          href="tel:+64211234567"
-          fullWidth
-        >
-          +64 21 123 4567
-        </Button>
-      </Stack>
-    </Box>
-  );
+return (
+<Stack direction="row" spacing={1}>
+<Button variant="contained" color="secondary" startIcon={<EmailIcon />} href="mailto:thomasperese@hotmail.com">
+Email Me
+</Button>
+<Button variant="outlined" color="secondary" startIcon={<LinkedInIcon />} href="https://linkedin.com/in/thomas-perese-140441190" target="_blank" rel="noreferrer">
+LinkedIn
+</Button>
+<Button variant="outlined" color="secondary" startIcon={<GitHubIcon />} href="https://github.com/thomasperese" target="_blank" rel="noreferrer">
+GitHub
+</Button>
+<Button variant="outlined" color="secondary" startIcon={<PhoneIcon />} href="tel:+16466761904">
+Phone +1 646 676 1904
+</Button>
+</Stack>
+);
 }
